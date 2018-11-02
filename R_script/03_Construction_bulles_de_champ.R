ffb9158296e68538b76858c9e05921d65f85c80e
AURELIE FRECHET <aurelie.frechet@gmail.com>
2018-11-02 12:02:22 +0100

    ajout des dates cles

--- R_script/03_Construction_bulles_de_champ.R
+++ R_script/03_Construction_bulles_de_champ.R
@@ -11,7 +11,7 @@ library(dplyr)
 library(lubridate)
 
 
-# Données 
+# Données
 data_tweets <- readRDS("data/data_tweets.RDS")
 
 
@@ -19,25 +19,37 @@ data_tweets <- readRDS("data/data_tweets.RDS")
 # Présence du mot "officiel" dans la description
 # data_tweets$officiel <- FALSE
 # data_tweets[grep(pattern = "officiel", x = data_tweets$description), "officiel"] <- TRUE
-# 
+#
 # # Présence du mot "actu" dans la description
 # data_tweets$actu <- FALSE
 # data_tweets[grep(pattern = "actu", x = data_tweets$description), "actu"] <- TRUE
-# 
+#
 # # Présence du mot "sport" dans la description
 # data_tweets$sport <- FALSE
 # data_tweets[grep(pattern = "sport", x = data_tweets$description), "sport"] <- TRUE
-# 
+#
 # # Présence du mot "journalist" dans la description
 # data_tweets$journalist <- FALSE
 # data_tweets[grep(pattern = "journalist", x = data_tweets$journalist), "journalist"] <- TRUE
-# 
+#
 # # variable segment
 # data_tweets = data_tweets %>% mutate(segment=ifelse(officiel==TRUE,"officiel",
 #                                                     ifelse(actu==TRUE,"actu",
 #                                                            ifelse(sport==TRUE,"sport",
 #                                                                   ifelse(journalist==TRUE,"journalist","twittos")))))
 
+# Temporaire
+data_tweets$segment <-
+  rep(
+    c(
+      "lambdas",
+      "politiques",
+      "medias",
+      "organisateurs",
+      "influenceurs"
+    ),
+    length = nrow(data_tweets)
+  )
 
 # 2 - Création de variables pour le tracé des points ----------------------
 
@@ -45,17 +57,26 @@ data_tweets <- readRDS("data/data_tweets.RDS")
 
 data_tweets %>% glimpse()
 
-base_tw = data_tweets %>% 
-  select (tweet_id,tweet_creation_dt,tweet_text,tweet_retweet_count,tweet_favorite_count,segment)
+base_tw = data_tweets %>%
+  select (
+    tweet_id,
+    tweet_creation_dt,
+    tweet_text,
+    tweet_retweet_count,
+    tweet_favorite_count,
+    segment
+  )
 
-ech = base_tw[1:1000,]
+# ech = base_tw[1:1000,]
 
 # Retravailler la date ----------------------------------------------------
 
-base_tw <- readRDS("data/data_tweets.RDS")
-base_tw$segment <- as.factor(rep(1:2, length = nrow(base_tw)))
 base_tw$tweet_creation_dt %>% class()
-base_tw$tweet_creation_dt = sort(as.POSIXlt(x = base_tw$tweet_creation_dt, tz = "GMT", format=("%d/%m/%Y %H:%M:%S")))
+base_tw$tweet_creation_dt = sort(as.POSIXlt(
+  x = base_tw$tweet_creation_dt,
+  tz = "GMT",
+  format = ("%d/%m/%Y %H:%M:%S")
+))
 base_tw$tweet_creation_dt %>% class()
 
 base_tw$tweet_date = as.Date(base_tw$tweet_creation_dt)
@@ -64,29 +85,82 @@ base_tw$tweet_date = as.Date(base_tw$tweet_creation_dt)
 base_tw %>% glimpse()
 # a - compter le nb de tweet par jour -------------------------------------
 maille_jour = base_tw %>%
-  select (tweet_date,tweet_id,tweet_text,tweet_retweet_count,tweet_favorite_count, segment) %>% 
-  group_by(tweet_date) %>% 
-  mutate(nb_tw_par_jour =n()) %>% 
+  select (
+    tweet_date,
+    tweet_id,
+    tweet_text,
+    tweet_retweet_count,
+    tweet_favorite_count,
+    segment
+  ) %>%
+  group_by(tweet_date) %>%
+  mutate(nb_tw_par_jour = n()) %>%
   ungroup()
 
 str(maille_jour)
-ech = maille_jour_tri[1:1000,]
+ech = maille_jour_tri[1:1000, ]
 
 
 # b - Trier par date et par retweet ----------------------------------------
-maille_jour_tri = maille_jour %>% 
-  arrange(tweet_date,tweet_retweet_count) 
+maille_jour_tri = maille_jour %>%
+  arrange(tweet_date, tweet_retweet_count)
 
 
 # c - boucle pour ordonner les tweetos ------------------------------------
-maille_jour_tri$count = unlist(lapply(table(maille_jour_tri$tweet_date), function(x){1:x}))
+maille_jour_tri$count = unlist(lapply(table(maille_jour_tri$tweet_date), function(x) {
+  1:x
+}))
 
 #Taille et coordonnées des points
 maille_jour_tri$size_point <-
-  floor((maille_jour_tri$tweet_retweet_count) / max(maille_jour_tri$tweet_retweet_count) * 200)+ 2
+  floor((maille_jour_tri$tweet_retweet_count) / max(maille_jour_tri$tweet_retweet_count) * 200 
+  ) + 0.1
 
-maille_jour_tri$coord <- unlist(
-  by(data = floor(maille_jour_tri$size_point/2)+1, maille_jour_tri$tweet_date, cumsum))
+maille_jour_tri$coord <- unlist(by(
+  data = floor(maille_jour_tri$size_point/2) + 1,
+  maille_jour_tri$tweet_date,
+  cumsum
+))
+
+
+
+#  d - dates clés ---------------------------------------------------------
+
+text <- c("23 juin 2015 - Paris annonce sa candidature aux JO 2024",
+          "15 septembre 2015 - Dépôt des candidatures : Paris, Los Angeles",
+          "9 février 2016 - Logo dévoilé sur l'Arc de Triomphe",
+          "3 février 2017 - Slogan dévoilé sur la Tour Eiffel",
+          "22 mars 2017 - Régions de France apporte leur soutien à la candidature",
+          "13-17 mai 2017 - La comission d'évaluation du CIO visite Paris",
+          "23-24 juin 2017 - Paris célèbre la journée internationale Olympique",
+          "11 juillet 2017 - La double attribution votée : Paris aura les Jeux",
+          "13 septembre 2017 - L'annonce officielle à Lima")
+
+class(maille_jour_tri$tweet_date)
+x <- as.Date(c("2015-06-23",
+               "2015-09-15",
+               "2016-02-09",
+               "2017-02-03",
+               "2017-03-22",
+               "2017-05-13",
+               "2017-06-23",
+               "2017-07-11",
+               "2017-09-13"))
+class(x)
+y <- rep(0, length(text))
+ax = c( )
+ay = c(50, 30, 10, 70, 70, 55, 40, 25, 10)
+xanchor = c("left",
+            "left",
+            "left",
+            "right",
+            "left",
+            "left",
+            "left",
+            "left",
+            "left")
+
+
 
 # 3 - Graphique -----------------------------------------------------------
 
@@ -103,17 +177,17 @@ maille_jour_tri$coord <- unlist(
 #     color=c("red","blue","green")
 #   )
 # )
-# 
-# 
-# maille_jour_tri %>% 
-#   ggplot(aes(x = tweet_date, y = count, col = segment)) + 
-#   geom_point() 
-# 
+#
+#
+# maille_jour_tri %>%
+#   ggplot(aes(x = tweet_date, y = count, col = segment)) +
+#   geom_point()
+#
 # ggplotly()
-# 
-# base = maille_jour_tri %>% 
+#
+# base = maille_jour_tri %>%
 #   dplyr::filter(tweet_date>= as.Date("2017-07-01") & tweet_date <= as.Date("2017-10-01"))
-# 
+#
 # plot_ly(
 #   x=maille_jour_tri$tweet_date,
 #   y=maille_jour_tri$count,
@@ -128,29 +202,63 @@ maille_jour_tri$coord <- unlist(
 
 # Plotly ------------------------------------------------------------------
 
-p <- plot_ly(maille_jour_tri, 
-             x = ~tweet_date, 
-             y = ~coord, 
-             # text = ~tweet_text, 
-             type = 'scattergl', 
-             mode = 'markers',
-             marker = list(size = ~size_point, 
-                           opacity = 0.5,
-                           color = 'none',
-                           line = list(
-                             color = ~segment,
-                             width = 12
-                           ))) %>%
-  layout(title = 'Tweets émis sur les JO2024',
-         xaxis = list(title = "",
-                      showgrid = FALSE,
-                      rangesilder = list(type = "date")),
-         yaxis = list(title = "",
-                      showgrid = FALSE,
-                      zeroline = FALSE,
-                      showline = FALSE,
-                      ticks = '',
-                      showticklabels = FALSE))
+maille_jour_tri$segment2 <-
+  ifelse(
+    maille_jour_tri$segment == "influenceurs",
+    "#0085C7",
+    ifelse(
+      maille_jour_tri$segment == "lambdas",
+      "#000000",
+      ifelse(
+        maille_jour_tri$segment == "medias",
+        "#F4C300",
+        ifelse(maille_jour_tri$segment == "politiques",
+               "#009F3D","#DF0024"
+        ))))
+
+p <- plot_ly(
+  maille_jour_tri,
+  x = ~ tweet_date,
+  y = ~ coord,
+  text = ~tweet_text,
+  type = 'scattergl',
+  mode = 'markers',
+  marker = list(
+    size = ~ size_point,
+    opacity = 0.5,
+    color = 'transparent',
+    line = list(
+      color = ~ segment2,
+      width = 3
+    )
+  )
+) %>%
+  layout(
+    title = 'Tweets émis sur les JO2024',
+    xaxis = list(
+      title = "",
+      showgrid = FALSE,
+      rangesilder = list(type = "date")
+    ),
+    yaxis = list(
+      title = "",
+      showgrid = FALSE,
+      zeroline = FALSE,
+      showline = FALSE,
+      ticks = '',
+      showticklabels = FALSE
+    ),
+    annotations = list(
+      x = x,
+      y = y,
+      text = text,
+      showarrow = TRUE,
+      arrowhead = 7,
+      xanchor = xanchor,
+      ax = y,
+      ay = ay
+    )
+  )
 
 p
 
@@ -166,10 +274,7 @@ p
 
 saveRDS(maille_jour_tri,  "data/trace_points.RDS")
 
-# Trace nuage 
+# Trace nuage
 # key : id du tweet
 # mots : lemmes du tweet
 # tweet_html : tweet au format html
-
-
-
